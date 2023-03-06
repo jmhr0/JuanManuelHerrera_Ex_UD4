a1567883eebf9e8b1622986e214e8284ae009f78
jmhr0 <jherram0603@g.educaand.es>
2023-03-06 14:32:09 +0100

    Examen terminado 14:32

---
+++ unit-testing-using-nunit/PrimeService/PrimeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            for (int i=1; i<= candidate; i++)
+            {
+                if (candidate % i==0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
